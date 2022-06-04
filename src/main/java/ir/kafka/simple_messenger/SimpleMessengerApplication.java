1102df567224d155242bcc42e46f71c53b2205e1
reza mehtari <remecom99@gmail.com>
2022-06-04 09:14:18 +0430

    init

---
+++ src/main/java/ir/kafka/simple_messenger/SimpleMessengerApplication.java
@@ -0,0 +1,13 @@
+package ir.kafka.simple_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMessengerApplication.class, args);
+    }
+
+}
